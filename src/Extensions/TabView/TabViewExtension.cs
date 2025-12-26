a63ac8dbf6e5a1bfa0e66544eac9eb19de8aa4f3
Pavlo4242 <pavlostepanchuk15@gmail.com>
2025-12-26 12:14:44 +0700

    about to change layout

--- src/Extensions/TabView/TabViewExtension.cs
+++ src/Extensions/TabView/TabViewExtension.cs
@@ -1,4 +1,3 @@
-
 using SwarmUI.Core;
 using SwarmUI.Utils;
 
@@ -9,8 +8,9 @@ public class TabViewExtension: Extension
     public override void OnInit()
     {
         // Register assets
-        StyleSheetFiles.Add("Assets/matti.css?v=15");
-        ScriptFiles.Add("Assets/matti.js?v=15");
+        ScriptFiles.Add("Assets/TabView.js?v=221");
+		StyleSheetFiles.Add("Assets/TabView.css?v=221");
+        
 
         // Add the tab HTML - but since it's file-based, assume it's loaded from Assets/Tabs/Text2Image/previewtab.html
         // No need to add here if using the folder structure
